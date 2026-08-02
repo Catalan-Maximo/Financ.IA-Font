2492d7e9029a3f8e194bfaaa9e843c9889be83b2
Maximo Benjamin Catalan <m.catalan@alumno.um.edu.ar>
2026-08-02 20:24:44 -0300

    fix: actualizar IP del backend a 192.168.100.99

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // REEMPLAZA ACA CON LA IP DE TU MAC QUE BUSCASTE EN EL PASO 8
-const MAC_IP = '192.168.100.77'; 
+const MAC_IP = '192.168.100.99'; 
 
 const api = axios.create({
   baseURL: `http://${MAC_IP}:8080/api/v1`,
